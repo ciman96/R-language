12da525e57c9952bb940fca05cc27fab3a82bf21
niihao <35862453+niihao@users.noreply.github.com>
2018-01-27 17:14:41 +0800

    Add files via upload

---
+++ DT2.R
@@ -0,0 +1,18 @@
+DT <- data.table(A = rep(letters[2:1], each = 4L), 
+                 B = rep(1:4, each = 2L), 
+                 C = sample(8))
+
+# Create the new data.table, DT2
+### doing j by group 
+
+DT[,.(A,B,C)]
+
+DT2 = DT[,.(C = cumsum(C)),by = .(A,B)]
+
+
+
+
+DT2[,.(C = tail(C,2)),by = A]
+
+#DT2，先按照A分組，再抓取C的最後兩筆資料並命名C
+

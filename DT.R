1224171d5bbf4c6dbc9a37077805289e97420c76
niihao <35862453+niihao@users.noreply.github.com>
2018-01-27 17:14:10 +0800

    Add files via upload

---
+++ DT.R
@@ -0,0 +1,27 @@
+DT1 = data.table(A=1:5,B=letters[1:5],C=6:10)
+
+DT2 = data.table(a = c(1L,2L), 
+                 b=LETTERS[1:4])
+
+
+#typeof(1) == "double"
+#typeof(1L) == "integer"
+
+#DT[i, j, by]
+
+
+DT1[,{print(A)
+      hist(A)
+      NULL}]
+
+DT1[.N]
+DT1[2:.N] #.N in row
+
+
+DT1[,.(B)]  #不降維度 return data.table
+DT1[,B]   #return vector
+
+DT = data.table(iris)
+DT[,.N,by = 10*round(Sepal.Length * Sepal.Width/10)]
+
+#Count how many occur in each group by specifying .N in j
